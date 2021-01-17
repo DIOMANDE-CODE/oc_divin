ec1728379b91eb8988a82955b48e636eff9e4aa0
PYTH <diomandedroh79@gmail.com>
2021-01-17 11:24:23 +0000

    recrutement4

--- oc_divin/settings.py
+++ oc_divin/settings.py
@@ -155,10 +155,11 @@ STATICFILES_DIRS = [
 
 # Configuration Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_HOST_USER="diomandedroh79@gmail.com"
 EMAIL_HOST_PASSWORD="PYTH79172617"
-EMAIL_USE_TLS=True
 EMAIL_PORT=587
 
 
