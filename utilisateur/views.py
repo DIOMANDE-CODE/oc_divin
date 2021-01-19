d412f5d30a3e1ee101dd05eb7b5f9a4567494a98
PYTH <diomandedroh79@gmail.com>
2021-01-19 00:03:46 +0000

    final3

--- utilisateur/views.py
+++ utilisateur/views.py
@@ -24,6 +24,10 @@ def prospect(request):
         diplome=request.POST['diplome']
         naissance=request.POST['naissance']
 
+        if Prospects.objects.get(email=email):
+            messages.info(request,"Cet email existe déja!")
+            return render(request,"inscription.html")
+
         if email=="" or nom=="" or prenom=="" or numero1=="" or numero2=="" or lieu=="" or sexe=="" or filiere=="" or diplome=="" or naissance=="":
             messages.info(request,"Tous les champs sont obligatoires")
             return render(request,"inscription.html")
